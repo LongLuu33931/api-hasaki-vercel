a19509d25638b6874a4dbdcc65c35ef0327adc09
LongLuu <64773511+LongLuu33931@users.noreply.github.com>
2023-07-08 14:09:18 +0700

    check user access token is existing

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,39 +1,39 @@
 import HttpStatusCode from "../exceptions/httpStatusCode.js";
 import jwt from "jsonwebtoken";
 import userAccessToken from "../models/userAccessToken.js";
-export default function checkToken(req, res, next) {
-  //bypass login, register
+
+export default async function checkToken(req, res, next) {
+  // Bypass login and register
   if (
-    req.url.toLowerCase().trim() == "/api/users/login".toLowerCase().trim() ||
-    req.url.toLowerCase().trim() == "/api/users/register".toLowerCase().trim()
+    req.url.toLowerCase().trim() === "/api/users/login" ||
+    req.url.toLowerCase().trim() === "/api/users/register"
   ) {
     next();
     return;
   }
-  //other requests
-  //get and validate token
+
+  // Other requests
+  // Get and validate token
   const token = req.headers?.authorization?.split(" ")[1];
-  const UAT = userAccessToken.findOne({ token }).exec();
-  console.log(typeof UAT);
-  debugger;
-  if (Object.keys(UAT).length === 0) {
-    res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
+
   try {
+    const UAT = await userAccessToken.findOne({ token }).exec();
+    if (Object.keys(UAT).length === 0) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+
     const jwtObject = jwt.verify(token, process.env.JWT_SECRET);
     const isExpired = Date.now() >= jwtObject.exp * 1000;
 
     if (isExpired) {
-      res.status(HttpStatusCode.BAD_REQUEST).json({
+      return res.status(HttpStatusCode.BAD_REQUEST).json({
         message: "Token is expired",
       });
-      res.end();
-    } else {
-      next();
-      return;
     }
+
+    next();
   } catch (exception) {
     res.status(401).json({
       message: "Unauthorized",
